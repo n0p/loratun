dabca0ae601015ddbf31ca73cee59bd9bfee50cd
xkrambler <xk19823d>
2018-08-01 18:41:33 +0100

    configuraciones más limpias

--- modem/test.c
+++ modem/test.c
@@ -4,11 +4,30 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-#define CONFIG(n, v) { Config c={n, v}; listNodeAdd(param, (Config *)&c); }
-
 int main() {
 
 	List *param=listNew();
+
+	void configAdd(char *k, char *v) {
+		Config *c=malloc(sizeof(Config));
+		strcpy(c->key, k);
+		strcpy(c->value, v);
+		listNodeAdd(param, (Config *)c);
+	}
+
+	void configDel(Config *c) {
+		free(c);
+	}
+
+	void configList() {
+		Node *n=param->first;
+		while (n) {
+			Config *c=(Config *)n->e;
+			printf("%s=%s\n", c->key, c->value);
+			//if (strcmp(c->key, "key2")==0) printf("*** parámetro me interesa!\n");
+			n=n->sig;
+		}
+	}
 
 	void sig_handler(int signo) {
 
@@ -19,10 +38,10 @@ int main() {
 		{
 			Node *n=param->first;
 			while (n) {
+				configDel((Config *)n->e);
 				listNodeDel(param, n);
 				n=n->sig;
 			}
-			printf("freed\n");
 		}
 		listFree(param);
 
@@ -47,29 +66,18 @@ int main() {
 	 * AT+JOIN 
 	 */
 
-	CONFIG("SerialPort", "/dev/ttyACM0");
-	CONFIG("AT+APPKEY", "ce:e3:fa:63:e5:ee:e8:ff:28:dd:08:69:ca:48:87:1c");
-	CONFIG("AT+NWSKEY", "cb:c9:a5:4e:de:35:4e:c9:33:61:cf:01:c3:71:e7:e2");
-	CONFIG("AT+DADDR", "06:c6:f5:c0");
-	CONFIG("AT+APPSKEY", "81:3f:2e:ad:c7:ec:ce:26:ae:b2:33:87:a9:b9:cb:45");
-	CONFIG("AT+ADR", "0");
-	CONFIG("AT+DR", "2");
-	CONFIG("AT+DCS", "0");
-	CONFIG("AT+TXP", "3");
-	CONFIG("AT+CLASS", "C");
-	CONFIG("AT+NJM", "1");
-
-	/*
-	{
-		Node *n=param->first;
-		while (n) {
-			Config *c=(Config *)n->e;
-			printf("key=%s value=%s\n", c->key, c->value);
-			if (strcmp(c->key, "key2")==0) printf("*** parámetro me interesa!\n");
-			n=n->sig;
-		}
-	}
-	*/
+	configAdd("SerialPort", "/dev/ttyACM0");
+	configAdd("AT+APPKEY", "ce:e3:fa:63:e5:ee:e8:ff:28:dd:08:69:ca:48:87:1c");
+	configAdd("AT+NWSKEY", "cb:c9:a5:4e:de:35:4e:c9:33:61:cf:01:c3:71:e7:e2");
+	configAdd("AT+DADDR", "06:c6:f5:c0");
+	configAdd("AT+APPSKEY", "81:3f:2e:ad:c7:ec:ce:26:ae:b2:33:87:a9:b9:cb:45");
+	configAdd("AT+ADR", "0");
+	configAdd("AT+DR", "2");
+	configAdd("AT+DCS", "0");
+	configAdd("AT+TXP", "3");
+	configAdd("AT+CLASS", "C");
+	configAdd("AT+NJM", "1");
+	configList();
 
 	if (signal(SIGINT,  sig_handler) == SIG_ERR) perror("\ncan't catch SIGINT\n");
 	if (signal(SIGQUIT, sig_handler) == SIG_ERR) perror("\ncan't catch SIGQUIT\n");
