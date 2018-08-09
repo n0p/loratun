48e4d4827b482126d5634abab3f7236fd2461651
n0p <lbernal@gmail.com>
2018-08-09 16:28:51 +0200

    Added TTN test app parameters

--- lorawan/test.c
+++ lorawan/test.c
@@ -47,10 +47,16 @@ int main() {
 	 */
 
 	modem_config_add("SerialPort", "/dev/ttyACM0");
-	modem_config_add("AT+APPKEY", "ce:e3:fa:63:e5:ee:e8:ff:28:dd:08:69:ca:48:87:1c");
-	modem_config_add("AT+NWSKEY", "cb:c9:a5:4e:de:35:4e:c9:33:61:cf:01:c3:71:e7:e2");
-	modem_config_add("AT+DADDR", "06:c6:f5:c0");
-	modem_config_add("AT+APPSKEY", "81:3f:2e:ad:c7:ec:ce:26:ae:b2:33:87:a9:b9:cb:45");
+	/* // App keys for pleiades gateway
+	configAdd("AT+APPKEY", "ce:e3:fa:63:e5:ee:e8:ff:28:dd:08:69:ca:48:87:1c");
+	configAdd("AT+NWSKEY", "cb:c9:a5:4e:de:35:4e:c9:33:61:cf:01:c3:71:e7:e2");
+	configAdd("AT+DADDR", "06:c6:f5:c0");
+	configAdd("AT+APPSKEY", "81:3f:2e:ad:c7:ec:ce:26:ae:b2:33:87:a9:b9:cb:45");
+	*/
+	// App keys for TTN test app
+	configAdd("AT+APPKEY", "35:4D:D6:24:10:56:7E:3C:8F:97:75:2C:EA:F4:05:F1");
+	configAdd("AT+APPEUI", "70:B3:D5:7E:D0:00:99:FB");
+	
 	modem_config_add("AT+ADR", "0");
 	modem_config_add("AT+DR", "7");
 	modem_config_add("AT+DCS", "0");
